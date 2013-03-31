5bbc40607b56202f54207fc3ad61ba0a399f0abb
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-03-31 15:55:13 +0200

    ok

--- test_ladspam.cc
+++ test_ladspam.cc
@@ -13,11 +13,15 @@ int main()
 	m1.set_port_value(0, 2, 99);
 	m1.set_port_value(0, 3, 0.9);
 	
+	m1.connect(0, 5, 1, 0);
+	
 	ladspam::m_jack m2("m2", 8);
 	m2.insert_plugin(0, "/usr/lib/ladspa/flanger_1191.so", "flanger");
 	m2.insert_plugin(0, "/usr/lib/ladspa/amp.so", "amp_mono");
 
-	usleep(1000000);
+	usleep(10000000);
+
+	m1.disconnect(0, 5, 1, 0);
 
 	m1.insert_plugin(0, "/usr/lib/ladspa/flanger_1191.so", "flanger");
 	m1.insert_plugin(m1.number_of_plugins(), "/usr/lib/ladspa/amp.so", "amp_mono");
