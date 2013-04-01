c771932b92a411040acadaadf2b04614dce3a466
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-04-01 13:32:08 +0200

    aha./ladspam-0-test-synth | gnuplot -p -e 'plot - with lines,- with lines'./ladspam-0-test-synth | gnuplot -p -e 'plot - with lines,- with lines'

--- test_synth.cc
+++ test_synth.cc
@@ -11,19 +11,16 @@ int main()
 {
 	ladspam::synth synth(48000, 8);
 	
-	synth.append_plugin("/usr/lib/ladspa/dahdsr_fexp.so", "dahdsr_fexp");
+	synth.append_plugin("/usr/lib/ladspa/sawtooth_1641.so", "sawtooth_fa_oa");
+	synth.append_plugin("/usr/lib/ladspa/sawtooth_1641.so", "sawtooth_fa_oa");
+	synth.append_plugin("/usr/lib/ladspa/sawtooth_1641.so", "sawtooth_fa_oa");
 	synth.append_plugin("/usr/lib/ladspa/product_1668.so", "product_iaia_oa");
-	synth.append_plugin("/usr/lib/ladspa/sawtooth_1641.so", "sawtooth_fa_oa");
-	synth.append_plugin("/usr/lib/ladspa/sawtooth_1641.so", "sawtooth_fa_oa");
+	synth.append_plugin("/usr/lib/ladspa/product_1668.so", "product_iaia_oa");
 
-	// Set the parameters for the envelope of voice 1
-	synth.set_port_value(0, 4, 0.2);
-	synth.set_port_value(0, 5, 0.2);
-	synth.set_port_value(0, 6, 0.2);
-	synth.set_port_value(0, 7, 0.2);
-	
-	synth.connect(2, 1, 1, 0);
-	synth.connect(3, 1, 1, 1);
+	synth.connect(0, 1, 3, 0);
+	synth.connect(1, 1, 3, 1);
+	synth.connect(3, 2, 4, 0);
+	synth.connect(2, 1, 4, 1);
 	
 	boost::timer::auto_cpu_timer t;
 	
@@ -33,7 +30,7 @@ int main()
 		
 		for (unsigned frame = 0; frame < 8; ++frame)
 		{
-			std::cout << (*synth.get_buffer(1, 2))[frame] << std::endl;
+			std::cout << (*synth.get_buffer(4, 2))[frame] << std::endl;
 		}
 	}
 }
