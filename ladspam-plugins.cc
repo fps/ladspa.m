85364fae9d88376cfd40b4c098be020953dd76b2
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-02-19 02:11:12 +0100

    add some plugins

---
+++ ladspam-plugins.cc
@@ -0,0 +1,137 @@
+#include <ladspa.h>
+#include <cmath>
+
+static const int LADSPAM_NUMBER_OF_PLUGINS = 3;
+static LADSPA_Descriptor ladspam_descriptors[LADSPAM_NUMBER_OF_PLUGINS];
+
+static const int LADSPAM_NUMBER_OF_PORTS = 5;
+
+static const char* ladspam_port_names[] = 
+{
+    "Frequency",
+    "Gate",
+    "Trigger",
+    "Phase",
+    "Out2"
+};
+
+static LADSPA_PortDescriptor ladspam_port_descriptors[] = 
+{
+    LADSPA_PORT_AUDIO | LADSPA_PORT_INPUT,
+    LADSPA_PORT_AUDIO | LADSPA_PORT_INPUT,
+    LADSPA_PORT_AUDIO | LADSPA_PORT_INPUT,
+    LADSPA_PORT_AUDIO | LADSPA_PORT_INPUT,
+    LADSPA_PORT_AUDIO | LADSPA_PORT_OUTPUT
+};
+
+static LADSPA_PortRangeHint ladspam_port_range_hints[] = 
+{
+    { LADSPA_HINT_SAMPLE_RATE | LADSPA_HINT_BOUNDED_BELOW | LADSPA_HINT_BOUNDED_ABOVE | LADSPA_HINT_DEFAULT_440, 0.0, 0.5 },
+    { LADSPA_HINT_BOUNDED_BELOW | LADSPA_HINT_BOUNDED_ABOVE | LADSPA_HINT_DEFAULT_MAXIMUM, 0.0, 1.0 },
+    { LADSPA_HINT_BOUNDED_BELOW | LADSPA_HINT_BOUNDED_ABOVE | LADSPA_HINT_DEFAULT_MINIMUM, 0.0, 1.0 },
+    { LADSPA_HINT_BOUNDED_BELOW | LADSPA_HINT_BOUNDED_ABOVE | LADSPA_HINT_DEFAULT_MINIMUM, 0.0, 1.0 },
+    { 0, 0.0, 0.0 }
+};
+
+struct ladspam_plugin
+{
+    float m_phase;
+
+    float *m_ports[LADSPAM_NUMBER_OF_PORTS];
+
+    unsigned long m_sample_rate;
+
+    ladspam_plugin(unsigned long sample_rate) :
+        m_phase(0),
+        m_sample_rate(sample_rate)
+    {
+
+    }
+};
+
+LADSPA_Handle ladspam_instantiate(const LADSPA_Descriptor *descriptor, unsigned long sample_rate)
+{
+    return new ladspam_plugin(sample_rate);    
+}
+
+void ladspam_cleanup(LADSPA_Handle instance)
+{
+    delete (ladspam_plugin*)instance;
+}
+
+void ladspam_connect_port(LADSPA_Handle instance, unsigned long port, LADSPA_Data * data_location)
+{
+    ((ladspam_plugin*)instance)->m_ports[port] = data_location;
+}
+
+void ladspam_run_sine(LADSPA_Handle instance, unsigned long sample_count)
+{
+    ladspam_plugin *i = (ladspam_plugin*)instance;
+    for (unsigned long index = 0; index < sample_count; ++index)
+    {
+        i->m_phase += i->m_ports[0][index] * 2.0 * M_PI / i->m_sample_rate;
+    }
+}
+
+void ladspam_run_sawtooth(LADSPA_Handle instance, unsigned long sample_count)
+{
+
+}
+
+void ladspam_run_square(LADSPA_Handle instance, unsigned long sample_count)
+{
+
+}
+
+const LADSPA_Descriptor* ladspa_descriptor(unsigned long Index)
+{
+    if (Index >= LADSPAM_NUMBER_OF_PLUGINS) return 0;
+
+    // static LADSPA_Descriptor *d = new LADSPA_Descriptor;
+    static LADSPA_Descriptor *d = &ladspam_descriptors[Index];
+
+    d->UniqueID = 0;
+
+    d->Properties = LADSPA_PROPERTY_REALTIME | LADSPA_PROPERTY_HARD_RT_CAPABLE;
+    d->Maker = "Florian Paul Schmidt";
+    d->Copyright = "GPL v2 or later";
+    d->PortCount = /* freq phase gate trigger */ 4 + /* out */ 1;    
+    
+    d->PortDescriptors = ladspam_port_descriptors;
+    d->PortRangeHints = ladspam_port_range_hints;
+
+    d->instantiate = ladspam_instantiate;
+    d->cleanup = ladspam_cleanup;
+    d->connect_port = ladspam_connect_port;
+
+    d->run_adding = 0;
+    d->set_run_adding_gain = 0;
+    d->activate = 0;
+    d->deactivate = 0;
+    
+    d->PortNames = ladspam_port_names;
+
+    d->ImplementationData = (void *)Index;
+
+    switch(Index)
+    {
+        case 0:
+            d->Label = "ladspam-sine-osc";
+            d->Name = "ladspam sine oscillator";
+            d->run = ladspam_run_sine;
+            break;
+        case 1:
+            d->Label = "ladspam-saw-osc";
+            d->Name = "ladspam sawtooth oscillator";
+            d->run = ladspam_run_sawtooth;
+            break;
+        case 2:
+            d->Label = "ladspam-square-osc";
+            d->Name = "ladspam square oscillator";
+            d->run = ladspam_run_square;
+            break;
+    }
+
+    return d;
+}
+
--- makefile
+++ makefile
@@ -6,11 +6,11 @@ SED ?= sed
 INCLUDE_PATH = $(PREFIX)/include/ladspam-0
 PKGCONFIG_DIR ?= $(PREFIX)/lib/pkgconfig
 
-CXXFLAGS ?= -O3 -march=native -DNDEBUG  -mfpmath=sse
+CXXFLAGS ?= -O3 -march=native -DNDEBUG  -mfpmath=sse -Wall
 
 .PHONY: install all clean docs
 
-all: ladspam-0-test-synth
+all: ladspam-0-test-synth ladspam-0-plugins.so
 
 LADSPAM_HEADERS = ladspam-0/synth.h 
 
@@ -23,6 +23,9 @@ install: all
 
 ladspam-0-test-synth: test_synth.cc ladspam-0/synth.h
 	g++ $(CXXFLAGS) -I .  -ansi -Wall -o ladspam-0-test-synth  test_synth.cc  `pkg-config ladspamm-0 --cflags --libs` -lboost_system -lboost_timer
+
+ladspam-0-plugins.so: ladspam-plugins.cc
+	g++ $(CXXFLAGS) -std=c++11 -shared -fPIC -o ladspam-0-plugins.so ladspam-plugins.cc
 
 docs:
 	doxygen
